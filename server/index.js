4b6eadff518f52314fba0fdaf6ada2025a374497
Ryan Rotman <ryan.rotman@gmail.com>
2021-04-25 14:34:10 -0500

    added middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 require("./db");
+const userRouter = require("./routers/user");
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+app.use(express.json());
+app.use(userRouter);
 
 app.get("/", (req, res) => {
     res.send("<h2>This is from index.js file</h2>")
