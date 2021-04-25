8c600195f77cf3718ff7b41e5695c27a3fdc0bda
Ryan Rotman <ryan.rotman@gmail.com>
2021-04-25 15:04:43 -0500

    pass resetUser prop to ThirdStep route

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -36,7 +36,7 @@ const AppRouter = () => {
                     />
                     <Route
                         render={(props) => (
-                            <ThirdStep {...props} user={user} />
+                            <ThirdStep {...props} user={user} updateUser={updateUser} resetUser={resetUser} />
                         )}
                         path="/third"
                     />
